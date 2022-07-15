87d1e7496c1e3e21936b4153d40ba000179c1083
singamraju167 <singamrajyalaxmi167@gmail.com>
2022-07-15 12:48:24 +0530

    hosting

--- core/urls.py
+++ core/urls.py
@@ -6,9 +6,16 @@ Copyright (c) 2019 - present AppSeed.us
 from django.contrib import admin
 from django.urls import path, include  # add this
 
+from django.views.static import serve
+from django.conf.urls import url
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
     path("", include("apps.authentication.urls")), # Auth routes - login / register
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
     # ADD NEW Routes HERE
 
