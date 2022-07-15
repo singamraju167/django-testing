836acf99c9059fe365634ffbe8800a65355c4e73
singamraju167 <singamrajyalaxmi167@gmail.com>
2022-07-15 18:51:06 +0530

    deleted login code

--- Procfile
+++
@@ -1,1 +0,0 @@
-web:gunicorn DTS.wsgi --log-file -
--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = env('SECRET_KEY', default='S#perS3crEt_007')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = env('DEBUG')
-DEBUG = False
+DEBUG = True
 
 # Assets Management
 ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets') 
